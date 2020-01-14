99bd87eb89e7ef0c17041786bb23e283db09bf5b
Rob Larsen <rob@htmlcssjavascript.com>
2020-01-14 16:21:40 -0500

    Triangle numbers (#4)
    * Generate Triangle Numbers

---
+++ triangle-numbes.js
@@ -0,0 +1,13 @@
+function triangle(num) {
+  return ((num) * (num + 1)) / 2;
+}
+function generateTriangleNumbers(num) {
+  let triangles = [];
+  for (let i = 1; i <= num; i++) {
+    triangles.push(triangle(i));
+  }
+  return triangles;
+}
+
+export {triangle, generateTriangleNumbers};
+
